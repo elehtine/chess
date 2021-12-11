b3fcdc5abf99f0765015aa7b70a564bc10c53860
kinnune <eljas.kinnunen98@gmail.com>
2021-12-11 17:03:36 +0200

    board and a king

--- src/header.h
+++ src/header.h
@@ -2,18 +2,28 @@
 
 typedef struct
 {
-	int col;
-	int row;
-	int type;
-	int is_white;
+    int col;
+    int row;
+    int type;
+    int is_white;
+    char *symbol;
 } piece;
 
 enum type
 {
-	pawn,
-	rook,
-	knight,
-	bishop,
-	queen,
-	king
+    pawn,
+    rook,
+    knight,
+    bishop,
+    queen,
+    king
 };
+
+//--board characters--
+char WHITE[] = "░";
+char BLACK[] = "█";
+char KING_W[] = "♔";
+char KING_B[] = "♚";
+char QUEEN_W[] = "♕";
+char QUEEN_B[] = "♛";
+//--------------------
--- src/main.c
+++ src/main.c
@@ -1,15 +1,54 @@
 #include "header.h"
 
-int		main(void)
+piece *test_coordinates(piece *pieces, int x, int y)
 {
-	piece rook1_w;
-	piece white[16];
-	piece black[16];
-	rook1_w.col = 0;
-	rook1_w.row = 7;
-	rook1_w.type = rook;
-	rook1_w.is_white = 1;
-	printf("[col %d row %d type %d colour %d]\n", rook1_w.col, rook1_w.row, rook1_w.type, rook1_w.is_white);
-	printf("Hello, world!");
-	return (0);
+    if (pieces[0].col == x && pieces[0].row == y)
+    {
+        return (&pieces[0]);
+    }
+    return (NULL);
+}
+
+void draw_board(piece *pieces)
+{
+    piece *in_coords;
+    int x;
+    int y;
+    for (y = 0; y < 8; y++)
+    {
+        for (x = 0; x < 8; x++)
+        {
+            in_coords = test_coordinates(pieces, x ,y);
+            if (in_coords)
+            {
+                printf("%s", in_coords->symbol);
+            }
+            else
+            {
+                if ((x % 2 == 0 && y % 2 == 0) || (x % 2 != 0 && y % 2 != 0))
+                {
+                    printf("%s", WHITE);
+                }
+                else
+                {
+                    printf("%s", BLACK);
+                }
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main(void)
+{
+    piece king_test;
+    piece pieces[32];
+    king_test.col = 4;
+    king_test.row = 7;
+    king_test.symbol = KING_W;
+    pieces[0] = king_test;
+    printf("[col %d row %d type %d colour %d]\n", king_test.col, king_test.row, king_test.type, king_test.is_white);
+    printf("Hello, world!\n");
+    draw_board(pieces);
+    return (0);
 }
