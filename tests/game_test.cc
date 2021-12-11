0470b210ae446d5bc413688ff2d7c48795c2b385
Elias Lehtinen <elias.lehtinen01@gmail.com>
2021-12-11 17:16:09 +0200

    First test stub for chess moves

---
+++ tests/game_test.cc
@@ -0,0 +1,23 @@
+#include <gtest/gtest.h>
+
+extern "C" {
+  // Include real game library
+  typedef struct
+  {
+    int col;
+    int row;
+    int type;
+    int is_white;
+  } piece;
+
+  int can_move(piece p, piece *board) {
+    return 1;
+  }
+}
+
+TEST(GameTest, KingMoves) {
+  piece board[32];
+  board[0] = { 7, 4, 5, 1 };
+  EXPECT_TRUE(can_move(board[0], board));
+  EXPECT_FALSE(can_move(board[0], board));
+}
